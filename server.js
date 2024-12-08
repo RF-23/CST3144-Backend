c74cdcce0d66e66e0d3c59bbfa92ae7aefeb432c
RF-23 <ridafarheen321@gmail.com>
2024-12-08 11:33:55 +0400

    Added all the backend files

---
+++ server.js
@@ -0,0 +1,4 @@
+// Import the required modules
+var express = require("express"); // Express framework for building web applications
+var app = express(); // Create an Express application instance
+const path = require("path"); // Node.js module to handle file and directory paths
