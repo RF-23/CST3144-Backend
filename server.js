42f3636b82e9e4c63a72df82a7670b7fde8435a9
RF-23 <ridafarheen321@gmail.com>
2024-12-08 11:43:04 +0400

    Added middleware to parse JSON data

--- server.js
+++ server.js
@@ -2,3 +2,8 @@
 var express = require("express"); // Express framework for building web applications
 var app = express(); // Create an Express application instance
 const path = require("path"); // Node.js module to handle file and directory paths
+
+// Middleware to parse JSON data from incoming requests
+app.use(express.json());
+
+
